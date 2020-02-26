d6cf2de8b397387208d585eae6099f6a4b36e874
choco <chocoh14t@gmail.com>
2020-02-26 21:13:47 +0900

    initial commit

---
+++ resources/js/config.example.js
@@ -0,0 +1,6 @@
+export default Config = {
+  algolia: {
+    appId: '',
+    key: ''
+  },
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>React Weather App</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="https://rawgithub.com/darkskyapp/skycons/master/skycons.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/places.js@1.18.1" defer></script>
+    <script src="{{asset('js/app.js')}}" defer></script>
+</head>
+<body class="bg-blue-200">
+    <div id="app" class="flex justify-center pt-16"></div>
+</body>
+</html>
